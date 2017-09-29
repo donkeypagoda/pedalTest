a849bd3a909ab3a1c51a3fb2b56efe668335ff2c
donkeypagoda <christopherallenstewart@gmail.com>
2017-09-29 15:23:42 -0700

    wut

--- app.js
+++ app.js
@@ -1,4 +1,48 @@
-(function() {
-  'use strict';
-  const ctx
-}());
+let time = document.querySelector('#delayTime');
+let feedbackSlider = document.querySelector("#delayFeedback");
+let delayBypass = document.querySelector("#delayBypass")
+let delayEngage = true;
+
+if (navigator.mediaDevices.getUserMedia) {
+  console.log("yah buddy getUserMedia is down with the plan");
+  navigator.mediaDevices.getUserMedia({audio: true})
+  .then ((stream) => {
+    let audioCtx = new AudioContext();
+    let source = audioCtx.createMediaStreamSource(stream);
+    let delay = audioCtx.createDelay();
+
+    let feedback = audioCtx.createGain();
+
+    feedback.gain.value = 0.0;
+
+    // routing:
+    if (delayEngage) {
+    source.connect(delay);
+    delay.connect(audioCtx.destination);
+    delay.connect(feedback);
+    feedback.connect(delay);
+    }
+    if (!delayEngage) {
+    source.connect(audioCtx.destination);
+    }
+
+    time.oninput = () => {
+      delay.delayTime.value = time.value;
+    };
+    feedbackSlider.oninput = () => {
+      // console.log(feedbackSlider.value);
+      feedback.gain.value = feedbackSlider.value;
+    };
+    delayBypass.onchange = () => {
+      delayEngage = !delayEngage
+    }
+
+
+  })
+  .catch(function(err) {
+        console.log('The following gUM error occured: ' + err);
+    });
+}
+else {
+    console.log('getUserMedia not supported round deez partz');
+}
