f86c1742480b4f9c0f195c13f15a652320c6fed8
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-05 10:14:24 -0700

    can't quite figure how to get the webaudioapi nodes exposed to be called on within the jquery IIFEs that kontrol seems to need

---
+++ disto.kontrol.js
@@ -0,0 +1,18 @@
+$(function() {
+  'use strict';
+
+  $("#distoSat")
+    .dial({
+      fgColor:"#222222",
+      bgColor:"#EEEEEE",
+      thickness: 0.3,
+      change : function (value) {
+          console.log("change : ", value);
+          disto1.curve = makeDistortionCurve(parseFloat(distoSat.value));
+      }
+
+      })
+    .css({display:'inline',padding:'0px 10px'});
+
+
+}());
