1f6411749c60b65d92ebfb2a47463fa1e5bb8912
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-06 14:21:03 -0700

    well it's working, it just can't change the panner speed dynamically

--- panner.js
+++ panner.js
@@ -5,45 +5,70 @@ class Panner {
     this.output = output;
     this.bypass = false;
     // UI STUFF XXXXXXXXXXXXXXXXXXXXXXXXX
-    this.pannerSpeed = document.querySelector("#pannerSpeed");
+    this.pannerSpeedInput = document.querySelector("#pannerSpeed");
+    this.pannerSpeed = 500;
     this.pannerWidth = document.querySelector("#pannerWidth");
     this.pannerStartStop = document.querySelector("#pannerStartStop");
+    this.pannerGo = true;
     this.pannerBypass = document.querySelector("#pannerBypass");
     // PANNER GUTS XXXXXXXXXXXXXXXXXXXXXXXXX
     this.panner = this.audioCtx.createStereoPanner();
     this.panner.pan.value = 0;
+
     this.pannerTable = Array.from(new Array(200), (x, i) => i/100 + -1.0);
-    this.pannerTableRev = Array.from(new Array(200), (x, i) => 1.0 - i/100);
-    console.log(this.pannerTableRev);
-    this.osc = this.audioCtx.createOscillator();
-    // this.wave = this.audioCtx.createPeriodicWave(this.pannerTable, this.pannerTableRev);
-    console.log(this.osc);
-    // console.log(wave);
-    // this.counter = 0;
+    this.pannerTableRev = Array.from(new Array(201), (x, i) => 1.0 - i/100);
+    this.pannerFullTable = this.pannerTable.concat(this.pannerTableRev)
+
+    this.counter = 0;
+
+    // ROUTING XXXXXXXXXXXXXXXXXXXXXXXXX
+    this.input.connect(this.panner)
+    this.panner.connect(this.output)
+
+    // THE ACTION XXXXXXXXXXXXXXXXXXXXXXXXX
+    this.panInc = () => {
+      this.panner.pan.value = this.pannerFullTable[this.counter++]
+      console.log(this.panner.pan.value)
+      if(this.counter === this.pannerFullTable.length - 1) {
+        this.counter = 0;
+        this.panInc();
+      }
+    }
+
+    if (this.pannerGo) {
+      this.intervalID = setInterval(this.panInc, this.pannerSpeed)
+    }
 
 
-    this.pannerSpeed.oninput = () => {
-      clearInterval();
-      setInterval(this.panInc, parseInt(this.pannerSpeed.value))
-      console.log(parseInt(this.pannerSpeed.value));
-      this.panInc = () => {
-        this.panner.pan.value = this.pannerTable[this.counter++]
-        // console.log(-1 + (counter++* 0.01));
-        console.log(this.panner.pan.value)
-        if(this.count === this.pannerTable.length - 1) {
-          this.counter = 0;
-          // return this.panInc();
-        }
+    this.pannerSpeedInput.oninput = () => {
+      this.pannerSpeed = parseInt(this.pannerSpeedInput.value);
+    }
+
+    this.pannerStartStop.onchange = () => {
+      this.pannerGo = !this.pannerGo;
+      if (this.pannerGo) {
+        this.intervalID = setInterval(this.panInc, parseInt(this.pannerSpeedInput.value))
+      }
+      else{
+        console.log(this.intervalID);
+        clearInterval(this.intervalID)
       }
     }
 
     this.pannerWidth.oninput = () => {
-      // this.panner.pan.value = parseFloat(this.pannerWidth.value)
-      // console.log(this.panner.pan.value);
+
     }
-    // ROUTING XXXXXXXXXXXXXXXXXXXXXXXXX
-    this.input.connect(this.panner)
-    this.panner.connect(this.audioCtx.destination)
+
+    this.pannerBypass.onchange = () => {
+      this.bypass = !this.bypass
+      if (this.bypass) {
+        this.input.connect(this.output)
+      }
+      else {
+        this.input.connect(this.panner)
+        this.panner.connect(this.output)
+      }
+    }
 
   }
 }
